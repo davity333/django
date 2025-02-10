86b7b587b23f71a208d6a91be4ea74d8fdf323a3
Héctor Somer <233424@ids.upchiapas.edu.mx>
2025-02-10 16:54:30 -0600

    usuarios

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('about', AboutPageView.as_view(), name= 'about'),
     path('login/',auth_views.LoginView.as_view(template_name = 'login.html'), name= 'login'),
     path('logout/',auth_views.LogoutView.as_view(template_name = 'logout.html'), name= 'logout'),
-
     path('proyect/', Proyect.as_view(), name= 'proyecto'),
     path('experiencia/', Experience.as_view(), name= 'experiencia'),
     path('user/', User.as_view(), name= 'user'),
