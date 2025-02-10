7534cee0cf8157cc6b5e70aa2c2b1e1ebab258b2
Héctor Somer <233424@ids.upchiapas.edu.mx>
2025-02-10 15:14:39 -0600

    si

--- tutorial/view.py
+++ tutorial/view.py
@@ -8,12 +8,23 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from .views.formUser import UserForm
+from .views.formProyecto import ProyectForm
 
 def index(request):
     return HttpResponse("Hello, World!")
 
 class Proyect(TemplateView):
     template_name = 'proyect.html'
+    def get(self, request):
+        form_instance = ProyectForm()
+        return render(request, self.template_name, {'form': form_instance})
+
+    def post(self, request):
+        form_instance = ProyectForm(request.POST)
+        if form_instance.is_valid():
+            form_instance.save()
+            return redirect('principal')  
+        return render(request, self.template_name, {'form': form_instance})
 
 class Experience(TemplateView):
     template_name = 'experience.html'
@@ -25,6 +36,7 @@ class User(TemplateView):
 class principal(TemplateView):
     template_name = 'principal.html'
 
+  
 class formLogin(TemplateView):  
     template_name = 'userForm.html'
 
